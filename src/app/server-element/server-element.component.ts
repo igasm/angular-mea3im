a20d5c03ce00e3b305dd5dcbec7447c673494269
igasm <iga.smarz@gmail.com>
2019-07-21 20:39:29 +0200

    view encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  //view encapsulation for CSS styles, emulated is default one anyway
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
   //property is bound to a DOM property in the template
